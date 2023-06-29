8cad7e337f7a2d6a9954d900a7bd133ec9f9779b
schultzm <schultzmoritz@gmail.com>
2023-06-29 16:18:05 +0200

    Wars das jetzt schon?

---
+++ src/de/tum/in/ase/eist/ThermoAdapter.java
@@ -0,0 +1,14 @@
+package de.tum.in.ase.eist;
+
+public class ThermoAdapter implements ThermoInterface {
+
+    FahrenheitThermo thermo;
+
+    public ThermoAdapter(){
+        thermo = new FahrenheitThermo();
+    }
+    public  double getTempC() {
+        double fahrenheit = thermo.getFahrenheitTemperature();
+        return (fahrenheit - 32.0) * 5.0 / 9.0;
+    }
+}
